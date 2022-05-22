ad99ae103c723a2954d2df8929fb1fe734531d8a
Hyunseok Byun <49316060+B-HS@users.noreply.github.com>
2022-05-22 13:38:45 +0900

    [Silver III] Title: 소수 최소 공배수, Time: 80 ms, Memory: 31352 KB -BaekjoonHub

--- 백준/Silver/21919. 소수 최소 공배수/소수 최소 공배수.py
+++ 백준/Silver/21919. 소수 최소 공배수/소수 최소 공배수.py
@@ -1,35 +1,10 @@
-import sys
-input = sys.stdin.readline
 sosu = 1
-fo = int(input())
+input()
 ary = set(map(int, input().split()))
-# for i in range(fo):
-#     num = ary[i]
-
-
 def sosuni(num):
-    if num in [2, 3, 5, 7]:
-        return True
     for i in range(2, int(k**0.5)+1):
-
-        if num % i == 0:
-            return False
+        if num % i == 0:return False
     return True
-
-
 for k in ary:
-    if sosuni(k) == True:
-        # for j in range(1, int(k**0.5)):
-        #     if k in [2, 3, 5, 7]:
-        #         # sosu.add(str(2))
-        #         sosu *= k
-        #         break
-        #     if k % j == 0:
-        #         break
-        #     # sosu.add(str(k))
-
-        sosu *= k
-
-
+    if sosuni(k) == True:sosu *= k
 print(sosu if sosu != 1 else -1)
-# print(eval("*".join(sosu)) if len(sosu) > 0 else -1)
