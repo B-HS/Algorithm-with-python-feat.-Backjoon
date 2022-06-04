f4dd1bf1d04e5cd6b8d64d39facae773d2b43cee
Hyunseok Byun <49316060+B-HS@users.noreply.github.com>
2022-06-05 04:50:52 +0900

    [Silver III] Title: 소수 최소 공배수, Time: 80 ms, Memory: 31352 KB -BaekjoonHub

--- 백준/Silver/21919. 소수 최소 공배수/소수 최소 공배수.py
+++ 백준/Silver/21919. 소수 최소 공배수/소수 최소 공배수.py
@@ -1,10 +1,10 @@
-sosu = 1
+a=1
 input()
-ary = set(map(int, input().split()))
-def sosuni(num):
-    for i in range(2, int(k**0.5)+1):
-        if num % i == 0:return False
+b=set(map(int, input().split()))
+def c(d):
+    for i in range(2,int(k**0.5)+1):
+        if d%i==0:return False
     return True
-for k in ary:
-    if sosuni(k) == True:sosu *= k
-print(sosu if sosu != 1 else -1)
+for k in b:
+    if c(k)==True:a*=k
+print(a if a!=1 else -1)
